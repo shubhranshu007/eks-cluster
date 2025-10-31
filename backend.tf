5a9fef25e39ffb304b2fa8763f3249fafd6f29f3
shubhranshu007 <44315013+shubhranshu007@users.noreply.github.com>
2025-10-31 16:06:48 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-fruitcombo"
-    key            = "terraform-s3-statefile-123/terraform.tfstate"
+    bucket         = "terraform-s3-statefile-123"
+    key            = "terraform.tfstate"
     region         = "us-east-1"
 #    dynamodb_table = "terraform-locks"
     encrypt        = true
