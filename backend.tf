7b83fa6cf2f48dc4f13d322e8c2a6f2ef0b2600c
shubhranshu007 <44315013+shubhranshu007@users.noreply.github.com>
2025-10-31 15:54:28 +0530

    Create backend.tf

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-fruitcombo"
+    key            = "bucket-terraform-demo-statefile/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+}
