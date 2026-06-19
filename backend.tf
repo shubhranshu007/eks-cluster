1020da4b83a5655c497d715a36a3bebbb7be3b4c
shubhranshu007 <44315013+shubhranshu007@users.noreply.github.com>
2026-06-19 22:49:30 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-s3-statefile-123"
+    bucket         = "terraform-s3-statefile-12345"
     key            = "env/dev/terraform.tfstate"
     region         = "us-east-1"
 #    dynamodb_table = "terraform-locks"
