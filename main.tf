15aef915c06deb4f14f499fa87da87bdc003f5ce
shubhranshu007 <44315013+shubhranshu007@users.noreply.github.com>
2026-06-20 11:31:51 +0530

    Reduce EKS node group scaling configuration

--- main.tf
+++ main.tf
@@ -109,8 +109,8 @@ resource "aws_eks_node_group" "node_group" {
   instance_types  = [var.node_instance_type]
 
   scaling_config {
-    desired_size = 2
-    max_size     = 2
+    desired_size = 1
+    max_size     = 1
     min_size     = 1
   }
 
