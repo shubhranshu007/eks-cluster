e9d1ddcc5baa3474752d3734ce249d4c877ad06c
shubhranshu007 <44315013+shubhranshu007@users.noreply.github.com>
2026-06-20 19:53:29 +0530

    Add IAM role policy attachment for SSM

--- ec2.tf
+++ ec2.tf
@@ -12,6 +12,7 @@ resource "aws_iam_role" "ssm_role" {
   })
 }
 
+
 resource "aws_iam_role_policy_attachment" "ssm_policy" {
   role       = aws_iam_role.ssm_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
